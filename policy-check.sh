c16b3bb235ea79a26a0d35f4ec6a32bcee96dbd7
PoWeiWu <tease0623@gmail.com>
2021-10-19 14:52:41 +0800

    modify check shell

--- policy-check.sh
+++ policy-check.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-check_data=$1
+# check_data=$1
 
-echo "$check_data"
+# echo "$check_data"
 
-# echo "terraform init"
-# terraform init
-echo "Output terraform plan json-formatted file"
-terraform plan --out tfplan.binary
+# # echo "terraform init"
+# # terraform init
+# echo "Output terraform plan json-formatted file"
+# terraform plan --out tfplan.binary
 
-# terraform show -json tfplan.binary > tfplan.json
-terraform show -json tfplan.binary > $check_data
+# # terraform show -json tfplan.binary > tfplan.json
+# terraform show -json tfplan.binary > $check_data
 
 # delete binary file
-rm -f tfplan.binary
+# rm -f tfplan.binary
 
 echo "Policy Check"
 # opa eval -d policy/ -i $check_data --fail-defined "data.terraform.gcp.instance.deny" --format pretty || check_state=$?
-conftest test $check_data --fail-on-warn -p policy/ --all-namespaces || check_state=$? 
+# conftest test $check_data --fail-on-warn -p policy/ --all-namespaces || check_state=$? 
 
 if [ "$check_state" = '2' ]
 then
